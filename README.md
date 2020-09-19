177d1247bd11d314710e7d3ffb1e41ca817dac72
Will Sutton <sutton_999@hotmail.com>
2020-09-19 11:34:52 +0100

    init commit testing a sample workflow with github actions & R

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 style="font-weight:normal">
+  TESTING GITHUB ACTIONS WITH R
+</h1>
---
+++ actions_workings.R
@@ -0,0 +1,7 @@
+# initial workings
+# using: https://ropenscilabs.github.io/actions_sandbox/
+
+devtools::install_github("r-lib/usethis")
+
+usethis::use_github_action_check_release()
+usethis::use_github_action_check_full()
